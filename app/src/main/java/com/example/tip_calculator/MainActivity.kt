03899773477ce5cfa42fd25107b9e660aca5b3d0
KoningHoningh <eelcohoningh@hotmail.com>
2022-06-29 18:10:31 +0200

    Added tests and some more design

--- app/src/main/java/com/example/tip_calculator/MainActivity.kt
+++ app/src/main/java/com/example/tip_calculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tip_calculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tip_calculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import java.util.*
@@ -15,16 +19,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) //sets binding to layoutInflater
         setContentView(binding.root) //binds binding to contentView from the Root
-        binding.calculateButton.setOnClickListener{ calculateTip()} //calls for the action to happen on clicking the calculate_button
+        binding.calculateButton.setOnClickListener {
+            hideKeyboard(it)
+            calculateTip()
+
+        } //calls for the action to happen on clicking the calculate_button
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+
+
     }
 
     //The action performed by clicking the button
     private fun calculateTip() {
-        val stringInTextField = binding.costOfServiceEditText.text.toString() //gets input from the input field
+        val stringInTextField =
+            binding.costOfServiceEditText.text.toString() //gets input from the input field
         val cost = stringInTextField.toDoubleOrNull() //converts it to either Null or a Double
         //determines wheter or not cost is Null
-        if (cost == null || cost == 0.0)
-        {
+
+
+
+        if (cost == null || cost == 0.0) {
             //Sets tip to 0.0
             displayTip(0.0)
             return
@@ -38,7 +54,8 @@ class MainActivity : AppCompatActivity() {
 
         var tip = cost * tipPercentage //calculates tip
         //if rounding up was selected, this will round the tip up.
-        if (binding.roundUpSwitch.isChecked) { tip = ceil(tip*5) / 5
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip * 5) / 5
         }
         //calls displayTip for the actual tip value
         displayTip(tip)
@@ -51,7 +68,23 @@ class MainActivity : AppCompatActivity() {
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
 
-
     }
 
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    private fun hideKeyboard(view: View) {
+        view.apply {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
 }
